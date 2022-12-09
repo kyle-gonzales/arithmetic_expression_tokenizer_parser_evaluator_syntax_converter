274189b1c62c2019906c5608245085d70df7e1c3
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2022-12-09 20:09:08 +0800

    supports unary '-' input

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ from evaluator_ import Evaluator
 
 # Input your expression here!
 
-s = "   ~123/ (11 + 32 / 23) + ((11/237+(169-36))*42) - ~92     "
-# s =  "(2+3*2)+((~2*5+8)*1)"
+# s = " (11 + 32 / -(-23+1))+ (-(11/237+(169-36))*42) - -92     "
+# s = "-(-1)"
+s =  "(2+3*2)+((-2*5+8)*1)"
 
 parser = Parser(s)
 ast = parser.get_ast()
-interpreter = Evaluator(ast)
+print(ast)
 
 if bool(ast):
     # print(infix_to_prefix(s))
     print(to_prefix(ast))
     # print(infix_to_postfix(s))
     print(to_postfix(ast))
+
+    interpreter = Evaluator(ast)
     print(interpreter.eval())
 
 else:
     print("Invalid Expression")
 
-# print(eval(str(ast)))
+print(eval(str(s)))
